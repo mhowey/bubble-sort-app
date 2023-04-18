efabeecf2a02cb39cb2946308e5b0cec5deff3a3
Matt Howey <howey.matt@gmail.com>
2023-04-18 15:50:39 -0400

    update tsconfig to ignore "sorter_versions" directory - add new NumbersCollection class - clean up

--- sorter_versions/index_83.ts
+++ sorter_versions/index_83.ts
@@ -1,5 +1,5 @@
 // importing a specific version of Sorter after lesson 83...
-import { Sorter } from "./Sorter_83";
+import { Sorter } from "../src/Sorter_89";
 
 const testCollection = [22, 100, 12, 12.4, -12, 11, 10, -2, 0, -5, 3];
 /** console.log input
@@ -9,8 +9,8 @@ const testCollection = [22, 100, 12, 12.4, -12, 11, 10, -2, 0, -5, 3];
  * ...which is exactly the same as the "testCollection" array above.
  */
 console.log("INPUT COLLECTION: ", [...testCollection]);
-const sorter = new Sorter(testCollection);
-sorter.sort();
+// const sorter = new Sorter(testCollection);
+// sorter.sort();
 
 /** console.log output
  * Here we'll console out the collection after it has been sorted:
@@ -18,5 +18,4 @@ sorter.sort();
  * `OUTPUT COLLECTION:  [-12, -5,  -2,  0, 3, 10,  11, 12, 12.4, 22, 100]
  * ...which is the correct sorting order we would expect from our bubble sort!
  */
-console.log("OUTPUT COLLECTION: ", sorter.collection);
-
+// console.log("OUTPUT COLLECTION: ", sorter.collection);
---
+++ src/NumbersCollection.ts
@@ -0,0 +1,62 @@
+/**
+ * properties here and constructor combined
+ */
+export class NumbersCollection extends Array {
+  constructor(public data: number[]) {
+    super();
+  }
+
+  get content() {
+    return this.data;
+  }
+
+  /** Getter for length property
+   * ! getter method
+   * ? What is "get" before the method name?
+   * :: the "get" keyword sets the function as a property "getter"
+   * :: so you can reference length from an instance of this class
+   * :: as something like:
+   * ` const collection = new NumberCollection([0, 6, 3, -1, 22, -4]);
+   * ` const length = collections.length;
+   * :: You call the length property to access it via the getter function
+   * :: to the end-user developer, this is just a property that is
+   * :: available instead of having to call a function to get the
+   * :: length, even though under the covers it IS calling a function.
+   */
+  get length() {
+    return this.data.length;
+  }
+
+  /** leftIndex and rightIndex
+   *
+   * ? What are "leftIndex" and "rightIndex" for?
+   * :: Given the following array of data that is being sorted:
+   * `["A", "b", "a", "B", "c", "D"]
+   * ::|_______| -> leftIndex = 0, rightIndex = 1;
+   * :: So yes, this could be shortened to only use leftIndex and
+   * :: simply add one, but to be explicit, let's leave both for DX.
+   * ! Note: tie-in with the original logic
+   * ? Which part of the original logic does this pertain to?
+   * ` this.collection[j] > this.collection[j + 1]
+   */
+  /**
+   *
+   * @param leftIndex
+   * @param rightIndex
+   * @returns boolean
+   */
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  /**
+   *
+   * @param leftIndex
+   * @param rightIndex
+   */
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftSide = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftSide;
+  }
+}
