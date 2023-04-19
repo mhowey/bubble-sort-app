3758a25201647106f48004ed5674cf57f972c061
Matt Howey <howey.matt@gmail.com>
2023-04-18 22:32:31 -0400

    Adds the CharacterCollection class that conforms to the ISortable interface

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -3,5 +3,31 @@
  * sure that is contains all the necessary properties of the ISortable interface
  */
 export class CharactersCollection {
-  constructor() {}
+  arrayOfStrings: string[];
+  constructor(public data: string) {
+    this.arrayOfStrings = this.data.split("");
+  }
+
+  get content() {
+    return this.data;
+  }
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return (
+      this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
+    );
+  }
+
+  // TODO: The following logic does not take into account capitals versus lowercase
+  // TODO: write logic to handle putting uppercase characters first
+  swap(leftIndex: number, rightIndex: number): void {
+    const saveLeft = this.arrayOfStrings[leftIndex];
+    this.arrayOfStrings[leftIndex] = this.arrayOfStrings[rightIndex];
+    this.arrayOfStrings[rightIndex] = saveLeft;
+    this.data = this.arrayOfStrings.join("");
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
 const arrayOfNumbers = [22, 100, 12, 12.4, -12, 11, 10, -2, 0, -5, 3];
+const unsortedString = "ZzzZwwWpqstabcefd";
 const numbersCollection = new NumbersCollection(arrayOfNumbers);
+const charactersCollection = new CharactersCollection(unsortedString);
 const collection = new NumbersCollection(numbersCollection);
 /** console.log input
  * Here we'll console out the initial collection value by spreading it into an Array
@@ -10,8 +13,14 @@ const collection = new NumbersCollection(numbersCollection);
  * `INPUT COLLECTION:  [22, 100, 12, 12.4, -12,  11, 10,   -2, 0,  -5,  3]
  * ...which is exactly the same as the "testCollection" array above.
  */
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+
+let sorterNumbersArr = new Sorter(numbersCollection);
+let sorterString = new Sorter(charactersCollection);
+
+sorterNumbersArr.sort();
+sorterString.sort();
+console.log("OUT arr", sorterNumbersArr.collection.content);
+console.log("OUT string", sorterString.collection.content);
 
 /** console.log output
  * Here we'll console out the collection after it has been sorted:
@@ -19,4 +28,3 @@ sorter.sort();
  * `OUTPUT COLLECTION:  [-12, -5,  -2,  0, 3, 10,  11, 12, 12.4, 22, 100]
  * ...which is the correct sorting order we would expect from our bubble sort!
  */
-console.log("OUTPUT COLLECTION: ", sorter.collection.content);
